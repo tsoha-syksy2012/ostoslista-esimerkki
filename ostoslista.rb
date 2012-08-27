153edaa86d36eed9825d591dbd92fd9a2098acff
Sami Saada <sami.saada@gmail.com>
2012-08-27 20:56:03 +0300

    kirjautuminen ohjaa etusivulle, jossa tulostetaan oletuslista

--- ostoslista.rb
+++ ostoslista.rb
@@ -1,10 +1,34 @@
 # encoding: utf-8
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './config/sequel'
 
 class Ostoslista < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+  end
+
+  configure do
+    enable :sessions
+  end
+
+  helpers do
+    def kirjautunut?
+      !session[:kayttaja].nil?
+    end
+
+    def kirjautunut_kayttaja
+      DB.fetch("SELECT id, username FROM users WHERE id = ?", session[:kayttaja]).first
+    end
+  end
+
+  get '/' do
+    unless kirjautunut?
+      redirect '/kirjautuminen'
+    else
+      lista = DB.fetch("SELECT * FROM lists WHERE user_id = ? AND is_default = true", kirjautunut_kayttaja[:id]).first
+      erb :lista, locals: {otsikko: "Ostoslista - #{lista[:name]}", kayttaja: kirjautunut_kayttaja, lista: lista}
+    end
   end
 
   get '/kirjautuminen' do
@@ -12,7 +36,13 @@ class Ostoslista < Sinatra::Base
   end
 
   post '/kirjautuminen' do
-    "lomake lähetetty"
+    kayttaja = DB.fetch("SELECT id FROM users WHERE username = ? AND password = ?", params[:tunnus], params[:salasana]).first
+    if kayttaja
+      session[:kayttaja] = kayttaja[:id]
+      redirect '/'
+    else
+      erb :kirjautumislomake, locals: {otsikko: 'Ostoslista - kirjautuminen', virhe: 'Kirjautuminen epäonnistui. Tarkista tunnus tai salasana!'}
+    end
   end
 
 end
