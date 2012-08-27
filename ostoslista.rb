fe04e1349b07f06220ddf4354946548e90a07b30
Sami Saada <sami.saada@gmail.com>
2012-08-27 19:41:37 +0300

    sivun runko luotu ja kirjautumislomake näkyy

--- ostoslista.rb
+++ ostoslista.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'sinatra/base'
 require 'sinatra/reloader'
 
@@ -5,4 +6,13 @@ class Ostoslista < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get '/kirjautuminen' do
+    erb :kirjautumislomake, locals: {otsikko: 'Ostoslista - kirjautuminen'}
+  end
+
+  post '/kirjautuminen' do
+    "lomake lähetetty"
+  end
+
 end
