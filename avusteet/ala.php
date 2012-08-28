e6e45915d809db27ed75c020bc864af876f1f843
Sami Saada <sami.saada@gmail.com>
2012-08-28 14:37:10 +0300

    kirjautumissivu

---
+++ avusteet/ala.php
@@ -0,0 +1,6 @@
+    <footer>
+      <p>Samin esimerkkitsoha, lähdekoodi: <a href="https://github.com/tsoha-syksy2012/ostoslista-esimerkki">Githubissa</a></p>
+      <p><a href="http://www.flickr.com/photos/lizatko/1351377013/" title="something is rotten in the state of... basket by katka koscova, on Flickr">Logon kuva</a></p>
+    </footer>
+  </body>
+</html>
---
+++ avusteet/yla.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="tyyli.css" />
+    <title><?php echo $otsikko; ?></title>
+  </head>
+  <body>
+    <img src="logo.jpg" title="Ostoslista" alt="Ostoslistan logo" />
+    <h1>Ostoslista</h1>
