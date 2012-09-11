2a62b346e53de7fccfdba9a51c3c5aea0cbec7d6
Sami Saada <sami.saada@gmail.com>
2012-09-11 15:55:18 +0300

    kirjautumislomakeen näyttäminen

---
+++ src/main/java/fi/cs/helsinki/saada/ostoslista/servletit/Kirjautuminen.java
@@ -0,0 +1,29 @@
+package fi.cs.helsinki.saada.ostoslista.servletit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author stb
+ */
+public class Kirjautuminen extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("kirjautumislomake.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+}
