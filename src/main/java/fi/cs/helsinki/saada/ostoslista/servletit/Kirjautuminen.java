7910a72ae0ee0fd9536d35b46733466a5b69e7b7
Sami Saada <sami.saada@gmail.com>
2012-09-12 16:13:16 +0300

    kirjautumisessa ohjataan listaan

--- src/main/java/fi/cs/helsinki/saada/ostoslista/servletit/Kirjautuminen.java
+++ src/main/java/fi/cs/helsinki/saada/ostoslista/servletit/Kirjautuminen.java
@@ -23,6 +23,7 @@ public class Kirjautuminen extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.sendRedirect("lista");
     }
 
 }
