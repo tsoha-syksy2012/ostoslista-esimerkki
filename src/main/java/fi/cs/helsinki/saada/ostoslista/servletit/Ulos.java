181f8635ec1461da9fe6647212da5aef2a17f387
Sami Saada <sami.saada@gmail.com>
2012-09-13 01:55:33 +0300

    uloskirjautuminen toiminnassa

---
+++ src/main/java/fi/cs/helsinki/saada/ostoslista/servletit/Ulos.java
@@ -0,0 +1,23 @@
+package fi.cs.helsinki.saada.ostoslista.servletit;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author stb
+ */
+public class Ulos extends OstoslistaServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        session.removeAttribute("kayttaja");
+        ohjaaSivulle("kirjautuminen", response);
+    }
+
+}
