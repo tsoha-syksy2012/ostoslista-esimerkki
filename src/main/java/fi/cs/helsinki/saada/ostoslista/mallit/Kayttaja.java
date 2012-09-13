c534e0669fb26bcdd3a89976b4ec8aef3d2e9c93
Sami Saada <sami.saada@gmail.com>
2012-09-13 16:47:50 +0300

    kirjautuminen kysyy käyttäjää

---
+++ src/main/java/fi/cs/helsinki/saada/ostoslista/mallit/Kayttaja.java
@@ -0,0 +1,15 @@
+package fi.cs.helsinki.saada.ostoslista.mallit;
+
+/**
+ *
+ * @author stb
+ */
+public class Kayttaja {
+
+    public static Kayttaja kirjauduSisaan(String tunnus, String salasana) {
+        System.out.println(tunnus);
+        System.out.println(salasana);
+        return null;
+    }
+
+}
--- src/main/java/fi/cs/helsinki/saada/ostoslista/servletit/Kirjautuminen.java
+++ src/main/java/fi/cs/helsinki/saada/ostoslista/servletit/Kirjautuminen.java
@@ -1,5 +1,6 @@
 package fi.cs.helsinki.saada.ostoslista.servletit;
 
+import fi.cs.helsinki.saada.ostoslista.mallit.Kayttaja;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,16 +16,28 @@ public class Kirjautuminen extends OstoslistaServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        asetaOtsikko("kirjautuminen", request);
-        palautaJSP("kirjautumislomake.jsp", request, response);
+        naytaLomake(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession(true);
-        session.setAttribute("kayttaja", new Long(1));
-        ohjaaSivulle("lista", response);
+        String tunnus = (String) request.getParameter("tunnus");
+        String salasana = (String) request.getParameter("salasana");
+        Kayttaja kayttaja = Kayttaja.kirjauduSisaan(tunnus, salasana);
+        if (kayttaja != null) {
+            session.setAttribute("kayttaja", new Long(1));
+            ohjaaSivulle("lista", response);
+        } else {
+            naytaLomake(request, response);
+        }
+    }
+
+    private void naytaLomake(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        asetaOtsikko("kirjautuminen", request);
+        palautaJSP("kirjautumislomake.jsp", request, response);
     }
 
 }
