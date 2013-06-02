99fe9f6bb6fdac938a213bc159f6c4abfcffaaa7
Sami Saada <sami.saada@gmail.com>
2013-06-02 11:27:06 +0300

    listan muokkaus

--- Handler/List.hs
+++ Handler/List.hs
@@ -2,6 +2,8 @@ module Handler.List (
   getListR,
   getListsR,
   postNewListR,
+  getEditListR,
+  postEditListR
 ) where
 
 import Import
@@ -27,4 +29,19 @@ postNewListR :: Handler ()
 postNewListR = do
   userId <- requireAuthId
   listId <- runDB $ insert $ List userId "Uusi lista" False
+  redirect $ ListR listId
+
+getEditListR :: ListId -> Handler RepHtml
+getEditListR listId = do
+  _ <- requireAuthId
+  list <- runDB $ get404 listId
+  defaultLayout $ do
+    setTitle $ toHtml $ listName list
+    $(widgetFile "edit_list")
+
+postEditListR :: ListId -> Handler ()
+postEditListR listId = do
+  _ <- requireAuthId
+  name <- runInputPost $ ireq textField "nimi"
+  runDB $ update listId [ListName =. name]
   redirect $ ListR listId
